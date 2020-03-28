0400b3b2872c3a3252dc8aadb0f949c59c1e7b47
Naumov1889storage <maksim.naumov1889@gmail.com>
2020-03-28 22:22:20 +0500

    .

--- dist/js/common.js
+++ dist/js/common.js
@@ -232,7 +232,7 @@
     if (Boolean(document.querySelector('.index-page-slider-section'))) {
         let n_of_slides = document.querySelectorAll('.index-page-slider__item').length;
 
-        for(let i=0; i<n_of_slides; i++) {
+        for (let i = 0; i < n_of_slides; i++) {
             let btn = document.createElement("button");
             document.querySelector('.index-page-slider__dots').appendChild(btn)
         }
@@ -243,5 +243,36 @@
             "controlsContainer": ".index-page-slider__arrows",
             "navContainer": ".index-page-slider__dots",
         });
+    }
+
+    if (Boolean(document.querySelector('.js-gallery-slider'))) {
+        if (window.innerWidth < 800) {
+            tns({
+                "container": ".js-gallery-slider",
+                "mouseDrag": true,
+                "navPosition": "bottom",
+                "loop": false,
+                "edgePadding": 20,
+                "gutter": 10,
+                // "items": 2,
+                "fixedWidth": 130,
+                responsive: {
+                    740: {
+                        "edgePadding": 20,
+                        "fixedWidth": 204
+                    },
+                    490: {
+                        "edgePadding": 20,
+                        "fixedWidth": 160,
+                        "gutter": 15,
+                    },
+                    360: {
+                        "edgePadding": 20,
+                        "fixedWidth": 140,
+                        "gutter": 15,
+                    },
+                }
+            });
+        }
     }
 }());
--- src/js/slider.js
+++ src/js/slider.js
@@ -2,7 +2,7 @@
     if (Boolean(document.querySelector('.index-page-slider-section'))) {
         let n_of_slides = document.querySelectorAll('.index-page-slider__item').length;
 
-        for(let i=0; i<n_of_slides; i++) {
+        for (let i = 0; i < n_of_slides; i++) {
             let btn = document.createElement("button");
             document.querySelector('.index-page-slider__dots').appendChild(btn)
         }
@@ -13,5 +13,36 @@
             "controlsContainer": ".index-page-slider__arrows",
             "navContainer": ".index-page-slider__dots",
         });
+    }
+
+    if (Boolean(document.querySelector('.js-gallery-slider'))) {
+        if (window.innerWidth < 800) {
+            tns({
+                "container": ".js-gallery-slider",
+                "mouseDrag": true,
+                "navPosition": "bottom",
+                "loop": false,
+                "edgePadding": 20,
+                "gutter": 10,
+                // "items": 2,
+                "fixedWidth": 130,
+                responsive: {
+                    740: {
+                        "edgePadding": 20,
+                        "fixedWidth": 204
+                    },
+                    490: {
+                        "edgePadding": 20,
+                        "fixedWidth": 160,
+                        "gutter": 15,
+                    },
+                    360: {
+                        "edgePadding": 20,
+                        "fixedWidth": 140,
+                        "gutter": 15,
+                    },
+                }
+            });
+        }
     }
 }());
